882d7d571ceb03a7691fb56040db4dfa80e976e4
blueface43 <63073069+blueface43@users.noreply.github.com>
2020-10-21 17:14:53 +0300

    ds

---
+++ src/First/Task1.java
@@ -0,0 +1,17 @@
+package First;
+
+import java.util.Scanner;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        float a,b,c,z;
+        System.out.println("Enter a,b,c: ");
+        a= scanner.nextFloat();
+        b= scanner.nextFloat();
+        c= scanner.nextFloat();
+        z=((a-3)*b/2)+c;
+        System.out.println("Value z="+z);
+    }
+}
